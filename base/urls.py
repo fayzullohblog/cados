05d0f4ace391a0bce906a5e07028a9121686baf9
portfolio1 <fayzulloh00010002@gmail.com>
2023-01-09 18:53:23 +0500

    get requesti orqiali icontainsqilish

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('',views.endpoint)
+    path('',views.endpoint),
+    path('username/<str:username>/',views.advocate_username)
 ]
--- base/views.py
+++ base/views.py
@@ -4,12 +4,24 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Advocate
 from .serializer import AdvocateSerializer
+from django.db.models import Q
 # Create your views here.
 @api_view(['GET'])
 def endpoint(request):
-    advocate=Advocate.objects.all()
+    query=request.GET.get('username')
+    bio=request.GET.get('bio')
+    print('---',query)
+    if  query==None:
+        query=''
+    elif bio == None:
+        bio=''
+
+    advocate=Advocate.objects.filter(username__icontains=query,bio__icontains=bio)
     serializer=AdvocateSerializer(advocate,many=True)
      
     return Response(data=serializer.data)
-
+@api_view(['GET'])
+def advocate_username(request,username):
+    advocate_username=[username]
+    return Response(advocate_username)
      
